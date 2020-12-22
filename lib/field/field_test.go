096c5acc4f7e12350e20c97e3e7fded092fced46
si-co <simone.colombo@epfl.ch>
2020-12-22 13:58:41 +0100

    Update field test

--- lib/field/field_test.go
+++ lib/field/field_test.go
@@ -15,12 +15,12 @@ func TestNewElement(t *testing.T) {
 	e := NewElement(x)
 	//fmt.Println(strconv.FormatUint(e.value.low, 2))
 	//fmt.Println(strconv.FormatUint(e.value.high, 2))
-	require.Equal(t, uint64(0x1), (e.value.low >> 63) & 1)
-	require.Equal(t, uint64(0x1), (e.value.low >> 59) & 1)
-	require.Equal(t, uint64(0x0), (e.value.low >> 58) & 1)
-	require.Equal(t, uint64(0x1), e.value.high & 1)
-	require.Equal(t, uint64(0x1), (e.value.high >> 6) & 1)
-	require.Equal(t, uint64(0x0), (e.value.high >> 7) & 1)
+	require.Equal(t, uint64(0x1), (e.value.low>>63)&1)
+	require.Equal(t, uint64(0x1), (e.value.low>>59)&1)
+	require.Equal(t, uint64(0x0), (e.value.low>>58)&1)
+	require.Equal(t, uint64(0x1), e.value.high&1)
+	require.Equal(t, uint64(0x1), (e.value.high>>6)&1)
+	require.Equal(t, uint64(0x0), (e.value.high>>7)&1)
 }
 
 func TestAdd(t *testing.T) {
@@ -44,7 +44,12 @@ func TestMul(t *testing.T) {
 	res := &Element{}
 	res.Mul(NewElement(x), NewElement(y))
 
-	require.Equal(t, "37856175e9dc9df26ebc6d6171aa0ae9", res.HexString())
+	expectedString := "37856175e9dc9df26ebc6d6171aa0ae9"
+	expected, err := hex.DecodeString(expectedString)
+	require.NoError(t, err)
+
+	require.Equal(t, expected, res.Bytes())
+	require.Equal(t, expectedString, res.HexString())
 }
 
 func TestSimpleMul(t *testing.T) {
