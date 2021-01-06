45d8fad451b61d74817d4fae495797b1d2240b0e
si-co <simone.colombo@epfl.ch>
2021-01-06 10:44:48 +0100

    Multi-bit scheme WIP

--- lib/client/it_multi.go
+++ lib/client/it_multi.go
@@ -1,6 +1,8 @@
 package client
 
 import (
+	"github.com/si-co/vpir-code/lib/constants"
+	cst "github.com/si-co/vpir-code/lib/constants"
 	"github.com/si-co/vpir-code/lib/field"
 	"golang.org/x/crypto/blake2b"
 )
@@ -26,7 +28,7 @@ type itMultiState struct {
 // NewITSingleGF return a client for the information theoretic multi-bit
 // scheme, working both with the vector and the rebalanced representation of
 // the database.
-func NewITSingleGF(xof blake2b.XOF, rebalanced bool) *ITSingleGF {
+func NewITMulti(xof blake2b.XOF, rebalanced bool) *ITSingleGF {
 	return &ITSingleGF{
 		xof:        xof,
 		rebalanced: rebalanced,
@@ -39,11 +41,71 @@ func NewITSingleGF(xof blake2b.XOF, rebalanced bool) *ITSingleGF {
 // on the client initialization.
 func (c *ITMulti) Query(index int, numServers int) [][]field.Element {
 	// TODO: check query inputs
+	blockLength = constants.BlockLength
 
 	// sample random alpha using blake2b
 	alpha := field.RandomXOF(c.xof)
+	alphaPrecomp := alpha.PrecomputeMul()
+
+	// set state
+	switch c.rebalanced {
+	case false:
+		// iy is unused if the database is represented as a vector
+		c.state = &itSingleGFState{
+			ix:       index,
+			alpha:    alpha,
+			dbLength: cst.DBLength,
+		}
+	case true:
+		panic("not yet implemented")
+	}
 
 	// compute vector a = (alpha, alpha^2, ..., alpha^b)
+	// TODO: simplify field API
+	a := make([]field.Element, blockLength)
+	a[0] = alpha
+	for i := range a[1:] {
+		e := &alpha
+		power := a[i-1].PrecomputeMul()
+		power.MulBy(e)
+		a[i] = *e
+	}
 
-	// compute vector a = (alpha, alpha^2, ..., alpha^b)
+	// additive secret sharing
+	eialpha := make([]field.Element, c.state.dbLength*(1+blockLength))
+	vectors := make([][]field.Element, numServers)
+
+	// create query vectors for all the servers
+	for k := 0; k < numServers; k++ {
+		vectors[k] = make([]field.Element, c.state.dbLength*(1+blockLength))
+	}
+
+	// zero vector in GF(2^128)^(1+b)
+	zeroVector := make([]field.Element, 1+blockLength)
+	for i := range zeroVector {
+		// TODO: can we use constant?
+		zeroVector[i] = field.Zero()
+	}
+
+	//numRandomElements := c.state.dbLength * (numServers - 1)
+	//randomElements := field.RandomVectorXOF(numRandomElements, c.xof)
+	for i := 0; i < c.state.dbLength; i++ {
+		// create basic vector
+		eialpha[i] = zero
+
+		// set alpha at the index we want to retrieve
+		if i == c.state.ix {
+			eialpha[i] = c.state.alpha
+		}
+
+		// create k - 1 random vectors
+		sum := field.Zero()
+		for k := 0; k < numServers-1; k++ {
+			rand := randomElements[c.state.dbLength*k+i]
+			vectors[k][i] = rand
+			sum = field.Add(sum, rand)
+		}
+		vectors[numServers-1][i] = field.Add(eialpha[i], sum)
+	}
+
 }
