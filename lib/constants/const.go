6adc01582daab47e8e218bd1cc717b746335c73d
si-co <simone.colombo@epfl.ch>
2021-01-15 13:54:08 +0100

    Add chunk bytes length to constants

--- lib/constants/const.go
+++ lib/constants/const.go
@@ -1,27 +1,17 @@
 package constants
 
 import (
-	"math"
-	"math/big"
-
 	"github.com/si-co/vpir-code/lib/field"
 )
 
 const (
 	DBLength             = 40000
-	FieldSize            = 64
 	BlockLength          = 16
+	ChunkBytesLength     = 15
 	SingleBitBlockLength = 0
 )
 
 var (
-	BigZero = big.NewInt(0)
-	BigOne  = big.NewInt(1)
-
-	// scheme parameters
-	Modulo = big.NewInt(int64(math.Pow(2, FieldSize)) - 1)
-	//Modulo = big.NewInt(math.MaxInt64)
-
 	Zero field.Element
 	One  field.Element
 )
