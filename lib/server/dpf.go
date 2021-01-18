3b4b7730b06f052011b64fdd672986b0309c41d4
si-co <simone.colombo@epfl.ch>
2021-01-18 12:27:31 +0100

    Update dpf Answer to new logic

--- lib/server/dpf.go
+++ lib/server/dpf.go
@@ -3,7 +3,6 @@ package server
 import (
 	"math/bits"
 
-	"github.com/si-co/vpir-code/lib/constants"
 	cst "github.com/si-co/vpir-code/lib/constants"
 	"github.com/si-co/vpir-code/lib/database"
 	"github.com/si-co/vpir-code/lib/dpf"
@@ -18,7 +17,7 @@ type DPF struct {
 	db *database.DB
 }
 
-func (s *DPF) Answer(q dpf.DPFkey, serverNum byte, blockSize int) []field.Element {
+func (s *DPF) Answer(q dpf.DPFkey, serverNum byte, blockSize int) [][]field.Element {
 	// Doing simplified scheme if block consists of a single bit
 	if blockSize == cst.SingleBitBlockLength {
 		panic("Not implemented")
@@ -40,32 +39,37 @@ func (s *DPF) Answer(q dpf.DPFkey, serverNum byte, blockSize int) []field.Elemen
 
 	// Part for the multi-bit scheme
 
+	// evaluate dpf
+	dpfOut := make([][]field.Element, s.db.NumRows)
+	for i := 0; i < len(dpfOut); i++ {
+		dpfOut[i] = make([]field.Element, s.db.BlockSize+1)
+	}
+
+	dpf.EvalFull(q, uint64(bits.Len(uint(s.db.NumRows))), dpfOut)
+
 	// compute the matrix-vector inner products
 	// addition and multiplication of elements
 	// in GF(2^128)^b are executed component-wise
-	m := make([]field.Element, blockSize)
-	tag := field.Zero()
+	var prodTag field.Element
+	m := make([][]field.Element, s.db.NumRows)
+	prod := make([]field.Element, s.db.BlockSize)
+	for i := 0; i < s.db.NumRows; i++ {
+		sumTag := field.Zero()
+		sum := field.ZeroVector(s.db.BlockSize)
+		m[i] = make([]field.Element, s.db.BlockSize)
+		for j := 0; j < s.db.NumColumns; j++ {
+			for b := 0; b < blockSize; b++ {
+				prod[b].Mul(&s.db.Entries[i][j][b], &dpfOut[i][0])
+				sum[b].Add(&sum[b], &prod[b])
 
-	dpfOut := make([][]field.Element, cst.DBLength)
-	for i := 0; i < len(dpfOut); i++ {
-		dpfOut[i] = make([]field.Element, blockSize+1)
-	}
-
-	dpf.EvalFull(q, uint64(bits.Len(uint(constants.DBLength))), dpfOut)
-
-	var tmp field.Element
-	for i := 0; i < len(dpfOut); i++ {
-		for j := 0; j < blockSize; j++ {
-			tmp.Mul(&s.db.Entries[i][j], &dpfOut[i][0])
-			m[j].Add(&m[j], &tmp)
-
-			tmp.Mul(&s.db.Entries[i][j], &dpfOut[i][j+1])
-			tag.Add(&tag, &tmp)
+				prodTag.Mul(&s.db.Entries[i][j][b], &dpfOut[i][b+1])
+				sumTag.Add(&sumTag, &prodTag)
+			}
 		}
+		m[i] = sum
+		// add tag
+		m[i] = append(m[i], sumTag)
 	}
-
-	// add tag
-	m = append(m, tag)
 
 	return m
 
