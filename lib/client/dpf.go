8a479959916c1324aeb14cfcd5548f997895024f
si-co <simone.colombo@epfl.ch>
2021-01-18 13:53:46 +0100

    Reconstruct dpf WIP, needs refactoring

--- lib/client/dpf.go
+++ lib/client/dpf.go
@@ -1,9 +1,11 @@
 package client
 
 import (
-	"github.com/si-co/vpir-code/lib/database"
+	"errors"
 	"io"
 	"math/bits"
+
+	"github.com/si-co/vpir-code/lib/database"
 
 	"github.com/si-co/vpir-code/lib/constants"
 	cst "github.com/si-co/vpir-code/lib/constants"
@@ -67,8 +69,59 @@ func (c *DPF) Query(index, numServers int) []dpf.DPFkey {
 	return []dpf.DPFkey{key0, key1}
 }
 
-func (c *DPF) Reconstruct(answers [][]field.Element) ([]field.Element, error) {
-	index := 0
+func (c *DPF) Reconstruct(answers [][][]field.Element) ([]field.Element, error) {
+	sum := make([][]field.Element, c.dbInfo.NumRows)
 
-	return reconstruct(answers, blockSize, index, c.state.alpha, c.state.a)
+	if c.dbInfo.BlockSize == cst.SingleBitBlockLength {
+		// sum answers as vectors in F^b
+		for i := 0; i < c.dbInfo.NumRows; i++ {
+			sum[i] = make([]field.Element, 1)
+			for k := range answers {
+				sum[i][0].Add(&sum[i][0], &answers[k][i][0])
+			}
+		}
+		for i := 0; i < c.dbInfo.NumRows; i++ {
+			if i == c.state.i {
+				switch {
+				case sum[i][0].Equal(&c.state.alpha):
+					return []field.Element{cst.One}, nil
+				case sum[i][0].Equal(&cst.Zero):
+					return []field.Element{cst.Zero}, nil
+				default:
+					return nil, errors.New("REJECT!")
+				}
+			} else {
+				if !sum[i][0].Equal(&c.state.alpha) && !sum[i][0].Equal(&cst.Zero) {
+					return nil, errors.New("REJECT!")
+				}
+			}
+		}
+	}
+
+	// sum answers as vectors in F^(b+1)
+	for i := 0; i < c.dbInfo.NumRows; i++ {
+		sum[i] = make([]field.Element, c.dbInfo.BlockSize+1)
+		for b := 0; b < c.dbInfo.BlockSize+1; b++ {
+			for k := range answers {
+				sum[i][b].Add(&sum[i][b], &answers[k][i][b])
+			}
+		}
+	}
+	var tag, prod field.Element
+	messages := make([]field.Element, c.dbInfo.BlockSize)
+	for i := 0; i < c.dbInfo.NumRows; i++ {
+		copy(messages, sum[i][:len(sum[i])-1])
+		tag = sum[i][len(sum[i])-1]
+		// compute reconstructed tag
+		reconstructedTag := field.Zero()
+		for b := 0; b < len(messages); b++ {
+			prod.Mul(&c.state.a[b], &messages[b])
+			reconstructedTag.Add(&reconstructedTag, &prod)
+		}
+		if !tag.Equal(&reconstructedTag) {
+			return nil, errors.New("REJECT")
+		}
+	}
+
+	return sum[c.state.i][:len(sum[c.state.i])-1], nil
 }
