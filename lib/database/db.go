028b9c3373fba7c753883d60b356ab09ac16a294
si-co <simone.colombo@epfl.ch>
2021-01-29 10:24:26 +0100

    db cleanup

--- lib/database/db.go
+++ lib/database/db.go
@@ -62,14 +62,13 @@ func CreateRandomMultiBitDB(rnd io.Reader, dbLen, numRows, blockLen int) *DB {
 
 func CreateRandomSingleBitDB(rnd io.Reader, dbLen, numRows int) *DB {
 	var tmp field.Element
-	var tmpb byte
 	entries := make([][]field.Element, numRows)
 	numColumns := dbLen / numRows
 	for i := 0; i < numRows; i++ {
 		entries[i] = make([]field.Element, numColumns)
 		for j := 0; j < numColumns; j++ {
 			tmp.SetRandom(rnd)
-			tmpb = tmp.Bytes()[len(tmp.Bytes())-1]
+			tmpb := tmp.Bytes()[len(tmp.Bytes())-1]
 			if tmpb>>7 == 1 {
 				entries[i][j].SetOne()
 			} else {
@@ -78,147 +77,6 @@ func CreateRandomSingleBitDB(rnd io.Reader, dbLen, numRows int) *DB {
 		}
 	}
 	return &DB{Entries: entries, Info: Info{NumColumns: numColumns, NumRows: numRows, BlockSize: 0}}
-}
-
-/*
-func CreateRandomMultiBitMatrix(rnd io.Reader, dbLen, blockLen int) *DB {
-	var err error
-	// compute square root of db length
-	dbLengthSqrt := math.Sqrt(float64(dbLen))
-	if dbLengthSqrt != math.Floor(dbLengthSqrt) {
-		log.Fatal(errors.New("square root of db length is not an integer"))
-	}
-	dbLengthSqrtInt := int(dbLengthSqrt)
-
-	numFieldElements := dbLengthSqrtInt / (field.Bytes * blockLen)
-	entries := make([][][]field.Element, numFieldElements)
-	for i := 0; i < dbLengthSqrtInt; i++ {
-		entries[i] = make([][]field.Element, numFieldElements)
-		for j := 0; j < dbLengthSqrtInt; j++ {
-			entries[i][j], err = field.RandomVector(rnd, blockLen)
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
-	}
-
-	return &DB{Entries: entries, NumColumns: dbLengthSqrtInt}
-}
-
-func CreateMultiBitGFLength(length int) *DB {
-	entries := make([][][]field.Element, constants.DBLength)
-	for i := range entries {
-		entries[i] = make([][]field.Element, 1)
-		for j := range entries[i] {
-			entries[i][j] = zeroVectorGF(length)
-		}
-	}
-
-	return &DB{Entries: entries}
-}
-
-func CreateMultiBitGF() *DB {
-	entries := make([][]field.Element, constants.DBLength)
-	one := field.One()
-	toRetrieve := []field.Element{one, one, one, one, one, one, one, one, one, one, one, one, one, one, one, one}
-	for i := range entries {
-		if i == 0 {
-			entries[i] = toRetrieve
-		} else {
-			entries[i] = zeroVectorGF(cst.BlockLength)
-		}
-	}
-
-	return &DB{Entries: entries}
-}
-
-func CreateVectorGF() *DB {
-	entries := make([][]field.Element, 1)
-	entries[0] = zeroVectorGF(cst.DBLength)
-
-	return &DB{Entries: entries}
-}
-
-func CreateAsciiVectorGF() *DB {
-	// playing with VPIR in ascii
-	db := CreateVectorGF()
-	for i, b := range text {
-		currentBit, err := strconv.Atoi(string(b))
-		if err != nil {
-			panic(err)
-		}
-		if currentBit == 0 {
-			val := field.Zero()
-			db.Entries[0][i] = val
-		} else {
-			val := field.One()
-			db.Entries[0][i] = val
-		}
-
-	}
-
-	return db
-}
-
-func CreateMatrixGF() *DB {
-	// compute square root of db length
-	dbLengthSqrt := math.Sqrt(cst.DBLength)
-	if dbLengthSqrt != math.Floor(dbLengthSqrt) {
-		panic(errors.New("Square root of db length is not an integer"))
-	}
-	dbLengthSqrtInt := int(dbLengthSqrt)
-
-	entries := make([][]field.Element, dbLengthSqrtInt)
-	for i := 0; i < dbLengthSqrtInt; i++ {
-		entries[i] = zeroVectorGF(dbLengthSqrtInt)
-	}
-
-	return &DB{Entries: entries, NumColumns: dbLengthSqrtInt}
-}
-
-func CreateAsciiMatrixGF() *DB {
-	// playing with VPIR in ascii
-	db := CreateMatrixGF()
-	for i, b := range text {
-		currentBit, err := strconv.Atoi(string(b))
-		if err != nil {
-			panic(err)
-		}
-		entry := field.Zero()
-		if currentBit == 1 {
-			entry = field.One()
-		}
-		db.Entries[i/db.NumColumns][i%db.NumColumns] = entry
-	}
-
-	return db
-}
-
-func CreateAsciiMatrixOneKb() *DB {
-	data := make([]byte, 1024)
-	rand.Read(data)
-	db := CreateMatrixGF()
-
-	bits := utils.Bytes2Bits(data)
-
-	for i, b := range bits {
-		entry := field.Zero()
-		if b == 1 {
-			entry = field.One()
-		}
-		db.Entries[i/db.NumColumns][i%db.NumColumns] = entry
-	}
-
-	return db
-}*/
-
-func zeroVectorGF(length int) []field.Element {
-	v := make([]field.Element, length)
-	for i := 0; i < length; i++ {
-		v[i] = field.Zero()
-	}
-
-	return v
 }
 
 /*
