b269f6c02c1315e35a54667741b731a399512af0
si-co <simone.colombo@epfl.ch>
2021-02-01 12:06:36 +0100

    Improve bash script

--- scripts/run_experiment.sh
+++ scripts/run_experiment.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+on_exit() {
+  # remove client pipe after experiment
+  rm client_pipe
+  # reset IFS
+  IFS=$2
+  exit
+}
+
+trap 'on_exit $client_pipe $OLDIFS' SIGTERM
+trap 'on_exit $client_pipe $OLDIFS' SIGINT
+
 INPUT=data/random_id_key.csv
 OLDIFS=$IFS
 IFS=','
@@ -7,7 +18,7 @@ IFS=','
 cd ..
 
 # create pipe to redirect ids later
-client_pipe=client_pipe_$RANDOM
+client_pipe=client_pipe
 mkfifo $client_pipe
 
 # launch client
@@ -19,7 +30,3 @@ while read id key
 do
   echo $id > $client_pipe
 done < $INPUT
-IFS=$OLDIFS
-
-# remove client pipe after experiment
-rm $client_pipe
