50cc32f5dbd9b38c3ebf88da865427a8c7420aa1
si-co <simone.colombo@epfl.ch>
2021-02-08 16:36:10 +0100

    Update experiments

--- experiments/plot.py
+++ experiments/plot.py
@@ -4,9 +4,16 @@ import json
 file="vpir_vector_oneMB.json"
 
 data = []
+query = []
+answer = []
+reconstruct = []
 with open(file) as f:
     data = json.load(f)
     for db_result in data['Results']:
-        print(db_result['BlockResults'])
+        for block_result in db_result['BlockResults']:
+            query.append(block_result['Query'])
+            answer.append(block_result['Answer0'])
+            answer.append(block_result['Answer1'])
+            reconstruct.append(block_result['Reconstruct'])
 
 
