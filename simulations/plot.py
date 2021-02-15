a83beaa17c30016890953d67ff37e62b155b533f
si-co <simone.colombo@epfl.ch>
2021-02-15 15:01:03 +0100

    Simulations WIP

--- simulations/plot.py
+++ simulations/plot.py
@@ -6,35 +6,20 @@ import matplotlib.pyplot as plt
 file="vpirSingleVector.json"
 
 # parse results
-data = []
-query = []
-answer = []
-reconstruct = []
 client = []
 server = []
 total = []
 with open(file) as f:
     data = json.load(f)
     for dbResult in data['Results']:
-        query.append(0)
-        answer.append(0)
-        reconstruct.append(0) 
         client.append(0)
         server.append(0)
         for blockResult in dbResult['Results']:
-            query[-1] += blockResult['Query']
-            answer[-1] += (blockResult['Answer0'] + blockResult['Answer1'])/2
-            reconstruct[-1] += blockResult['Reconstruct']
-
             client[-1] += blockResult['Query'] + blockResult['Reconstruct']
             server[-1] += (blockResult['Answer0'] + blockResult['Answer1'])/2
         total.append(dbResult['Total'])
 
 # compute averages
-queryAvg = np.mean(query)
-answerAvg = np.mean(answer)
-reconstructAvg = np.mean(reconstruct)
-
 clientAvg = np.mean(client)
 clientStd = np.std(client)
 serverAvg = np.mean(server)
@@ -47,6 +32,7 @@ labels = ['1KB']
 client_means = [clientAvg]
 server_means = [serverAvg]
 client_std = [clientStd]
+print(clientStd)
 server_std = [serverStd]
 width = 0.35       # the width of the bars: can also be len(x) sequence
 
