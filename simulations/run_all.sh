4449d08f6ff217d9ca66cfd4b07beb499aa66a46
si-co <simone.colombo@epfl.ch>
2021-02-16 10:02:15 +0100

    Simul WIP

--- simulations/run_all.sh
+++ simulations/run_all.sh
@@ -6,5 +6,7 @@ mkdir -p results
 # run experiments
 make -s run_simul config=vpirSingleVector.toml
 make -s run_simul config=vpirMultiVector.toml
+make -s run_simul config=vpirSingleMatrix.toml
+make -s run_simul config=vpirMultiMatrix.toml
 
 # produce plots
