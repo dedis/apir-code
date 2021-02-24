f8d0acc4aff50d3988d9e2a24793ef940433d8d2
si-co <simone.colombo@epfl.ch>
2021-02-24 17:43:46 +0100

    Update

--- lib/database/merkle.go
+++ lib/database/merkle.go
@@ -1,18 +1,17 @@
 package database
 
 import (
-	"bytes"
-	"crypto/sha256"
 	"fmt"
 	"io"
+	"log"
 
 	"github.com/si-co/vpir-code/lib/field"
-
-	"gitlab.com/NebulousLabs/merkletree"
+	merkletree "github.com/wealdtech/go-merkletree"
 )
 
 type Merkle struct {
-	db *Bytes
+	DB   *Bytes
+	Root []byte
 }
 
 // CreateRandomMultiBitMerkle
@@ -20,27 +19,77 @@ type Merkle struct {
 // case
 func CreateRandomMultiBitMerkle(rnd io.Reader, dbLen, numRows, blockLen int) *Merkle {
 	db := CreateRandomMultiBitBytes(rnd, dbLen, numRows, blockLen)
-	entriesFlatten := flatten(db.Entries)
 
-	for i, _ := range entriesFlatten {
-		r := bytes.NewReader(entriesFlatten)
-		merkleRoot, proof, numLeaves, err := merkletree.BuildReaderProof(r, sha256.New(), blockLen*field.Bytes, uint64(i))
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println(merkleRoot, proof, numLeaves)
+	// a leaf contains a block, which has the same numbers of bytes as a
+	// block composed of field elements
+	blockLenBytes := blockLen * field.Bytes
+	fmt.Println("blockLenblockLenBytes:", blockLenBytes)
+	fmt.Println(blockLenBytes)
+	blocks := entriesToBlocks(db.Entries, blockLenBytes)
+	tree, err := merkletree.New(blocks)
+	if err != nil {
+		log.Fatalf("impossible to create Merkle tree: %v", err)
 	}
 
+	// getch the root hash of the tree
+	//root := tree.Root()
+
+	// generate and encode all the proofs
+	proofs := make([]byte, len(blocks))
+	for i, b := range blocks {
+		proofs[i], err = tree.GenerateProof(b)
+		if err != nil {
+			log.Fatalf("impossible to generate proof for block %v: %v", b, err)
+		}
+
+	}
+	fmt.Println(proofs)
+
+	//// hash function for the Merkle tree
+	//h := sha256.New()
+
+	//// compute the proof for each block
+	//var mr []byte
+	//proofs := make([][]byte, len(entriesFlatten))
+	//for i, _ := range entriesFlatten {
+	//r := bytes.NewReader(entriesFlatten)
+	//merkleRoot, proof, _, err := merkletree.BuildReaderProof(r, h, segmentSize, uint64(i))
+	//if err != nil {
+	//panic(err)
+	//}
+	//fmt.Println(proof)
+	//proofs[i] = flatten(proof)
+	//mr = merkleRoot // always the same
+	//}
+
+	//m := &Merkle{
+	//DB:         db,
+	//MerkleRoot: mr,
+	//}
+
+	//// enlarge the db by adding a proof to each block
+	//enlargedEntries := make([][]byte, 0) // TODO: specify length
+	//for i := range db.Entries {
+	//for j := 0; j < db.Entries[0]; j += segmentSize {
+	//a = append(a[:i], append(make([]T, j), a[i:]...)...)
+	//append(enlargedEntries[i][:j], append(proofs[i*len(db.Entries[0])+j], enlar
+	//}
+	//}
+
+	//return m
 	return nil
 }
 
-func flatten(m [][]byte) []byte {
-	out := make([]byte, len(m)*len(m[0]))
-	for i := range m {
-		for j := range m[0] {
-			out[i*len(m)+j] = m[i][j]
+func entriesToBlocks(e [][]byte, blockLength int) [][]byte {
+	blocks := make([][]byte, len(e)*len(e[0])/blockLength)
+	b := 0
+	for i := range e {
+		for j := 0; j < len(e[0])-blockLength; j += blockLength {
+			blocks[b] = e[i][j : j+blockLength]
+			fmt.Println(len(blocks[b]))
+			b++
 		}
 	}
 
-	return out
+	return blocks
 }
