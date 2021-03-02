912c3d5699e59a330b5a01e7d71419837e594e32
Kirill Nikitin <kirill.nikitin@epfl.ch>
2021-03-02 11:31:15 +0100

    WIP: parallelizing the server, stuck in an endless loop so far

--- lib/server/server.go
+++ lib/server/server.go
@@ -4,6 +4,8 @@ import (
 	cst "github.com/si-co/vpir-code/lib/constants"
 	"github.com/si-co/vpir-code/lib/database"
 	"github.com/si-co/vpir-code/lib/field"
+	"runtime"
+	"sync"
 )
 
 // Server is a scheme-agnostic VPIR server interface, implemented by both IT
@@ -36,32 +38,60 @@ func answer(q []field.Element, db *database.DB) []field.Element {
 		copy(qOne[j*db.BlockSize:(j+1)*db.BlockSize], q[j*(db.BlockSize+1)+1:(j+1)*(db.BlockSize+1)])
 	}
 
+	// multithreading
+	var wg sync.WaitGroup
+	// channel to pass the results from the routines back
+	results := make([]chan []field.Element, db.NumRows)
+	numCPU := runtime.NumCPU()
+	var colChunkLen int = db.NumColumns / numCPU
 	// compute the matrix-vector inner products
 	// addition and multiplication of elements
 	// in DB(2^128)^b are executed component-wise
-	var prodTag, prod field.Element
 	m := make([]field.Element, db.NumRows*(db.BlockSize+1))
 	// we have to traverse column by column
 	for i := 0; i < db.NumRows; i++ {
-		sumTag := field.Zero()
-		sum := field.ZeroVector(db.BlockSize)
-		for j := 0; j < db.NumColumns; j++ {
-			for b := 0; b < db.BlockSize; b++ {
-				if db.Entries[i][j*db.BlockSize+b].IsZero() {
-					// no need to multiply is the the element value is zero
-					continue
-				}
-				prod.Mul(&db.Entries[i][j*db.BlockSize+b], &qZeroBase[j])
-				sum[b].Add(&sum[b], &prod)
-
-				prodTag.Mul(&db.Entries[i][j*db.BlockSize+b], &qOne[j*db.BlockSize+b])
-				sumTag.Add(&sumTag, &prodTag)
-			}
+		for c := 0; c < numCPU; c++ {
+			wg.Add(1)
+			go processChunk(db.Entries[i][c*colChunkLen*db.BlockSize:(c+1)*colChunkLen*db.BlockSize], db.BlockSize,
+				qZeroBase[c*colChunkLen:(c+1)*colChunkLen], qOne[c*colChunkLen*db.BlockSize:(c+1)*colChunkLen*db.BlockSize], results[i], &wg)
 		}
-		copy(m[i*(db.BlockSize+1):(i+1)*(db.BlockSize+1)-1], sum)
-		// add tag
-		m[(i+1)*(db.BlockSize+1)-1].Set(&sumTag)
+		go combineChunkResults(m[i*(db.BlockSize+1):(i+1)*(db.BlockSize+1)], results[i])
+	}
+	wg.Wait()
+	for i := range results {
+		close(results[i])
 	}
 
 	return m
+}
+
+func processChunk(dbChunk []field.Element, blockLen int, qZ []field.Element, qO []field.Element,
+																	reply chan<- []field.Element, wg *sync.WaitGroup) {
+	var prodTag, prod field.Element
+	sumTag := field.Zero()
+	sum := field.ZeroVector(blockLen)
+	for j := 0; j < len(dbChunk)/blockLen; j++ {
+		for b := 0; b < blockLen; b++ {
+			if dbChunk[j*blockLen+b].IsZero() {
+				// no need to multiply is the the element value is zero
+				continue
+			}
+			prod.Mul(&dbChunk[j*blockLen+b], &qZ[j])
+			sum[b].Add(&sum[b], &prod)
+
+			// compute block tag
+			prodTag.Mul(&dbChunk[j*blockLen+b], &qO[j*blockLen+b])
+			sumTag.Add(&sumTag, &prodTag)
+		}
+	}
+	reply <- append(sum, sumTag)
+	wg.Done()
+}
+
+func combineChunkResults(product []field.Element, workerReplies <-chan []field.Element) {
+	for reply := range workerReplies {
+		for i, elem := range reply {
+			product[i].Add(&product[i], &elem)
+		}
+	}
 }
