bda23e29512a79cf358478115ef47c208fb3f453
si-co <simone.colombo@epfl.ch>
2021-03-12 14:35:47 +0100

    Add script for real simulation

---
+++ simulations/real.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+GOGC=8000
+
+# build server
+cd ../cmd/server
+go build -race
+
+# build client
+cd ../client
+go build -race
+
+# move to root
+cd ../../
+
+# run servers
+env GOGC=$GOGC go run cmd/server/main.go -id=0 -files=1 &
+pid0=$!
+env GOGC=$GOGC go run cmd/server/main.go -id=1 -files=1 &
+pid1=$!
+
+# run client
+env GOGC=$GOGC go run cmd/client/main.go -id=alex.braulio@varidi.com
