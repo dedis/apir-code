b98b017d920cc8309265499ea5b3dc44268effb9
si-co <simone.colombo@epfl.ch>
2021-03-22 16:38:05 +0100

    Starting with AWS

--- cmd/aws/server/main.go
+++ cmd/aws/server/main.go
@@ -2,25 +2,36 @@ package main
 
 import (
 	"context"
+	"encoding/base64"
 	"fmt"
+	"log"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+var database *database.DB
+
 func main() {
-	lambda.Start(HandleRequest)
+	// prepare the database
+	lambda.Start(HandleQuery)
 }
 
 // HandleRequest handles the lambda request.
-func HandleRequest(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+func HandleQuery(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	// b64 encoded query
-	query := req.Body
+	queryEncoded := req.Body
 
 	// decode b64 query
+	query, err := base64.StdEncoding.DecodeString(queryEncoded)
+	if err != nil {
+		log.Fatalf("impossible to decode the query: %v", err)
+	}
+
+	// run answer
 
 	return events.APIGatewayProxyResponse{
-		Body:       fmt.Sprintf(`{"query": "%s"}`, body),
+		Body:       fmt.Sprintf(`{"answer": "%s"}`, body),
 		StatusCode: 200,
 		Headers:    map[string]string{"Content-Type": "application/json"},
 	}, nil
