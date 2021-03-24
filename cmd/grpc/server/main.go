701b67dd5b4da4789583a3d9239b51129c330db7
si-co <simone.colombo@epfl.ch>
2021-03-24 13:59:16 +0100

    Prepare gRPC server for experiments

--- cmd/grpc/server/main.go
+++ cmd/grpc/server/main.go
@@ -52,6 +52,18 @@ func main() {
 		log.SetOutput(f)
 	}
 
+	// set stats log
+	var statsLogger *log.Logger
+	if *experiment {
+		fileName := fmt.Sprintf("stats_server-%v.log", *sid)
+		f, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE|os.O_CREATE, 0666)
+		if err != nil {
+			log.Fatalf("could not open stats.log file: %v", err)
+		}
+		defer f.Close()
+		statsLogger = log.New(f, "", log.Lmsgprefix)
+	}
+
 	// configs
 	config, err := utils.LoadConfig("config.toml")
 	if err != nil {
@@ -89,13 +101,17 @@ func main() {
 	}
 
 	// start server
-	proto.RegisterVPIRServer(rpcServer, &vpirServer{Server: s})
+	proto.RegisterVPIRServer(rpcServer, &vpirServer{
+		Server:      s,
+		experiment:  *experiment,
+		cores:       *cores,
+		statsLogger: statsLogger,
+	})
 	log.Printf("is listening at %s", addr)
 
 	// listen signals from os
 	sigCh := make(chan os.Signal, 1)
 	signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
-
 	errCh := make(chan error, 1)
 
 	go func() {
@@ -119,6 +135,11 @@ func main() {
 type vpirServer struct {
 	proto.UnimplementedVPIRServer
 	Server server.Server // both IT and DPF-based server
+
+	// only for experiments
+	experiment  bool
+	cores       int
+	statsLogger *log.Logger
 }
 
 func (s *vpirServer) DatabaseInfo(ctx context.Context, r *proto.DatabaseInfoRequest) (
@@ -143,7 +164,11 @@ func (s *vpirServer) Query(ctx context.Context, qr *proto.QueryRequest) (
 	if err != nil {
 		return nil, err
 	}
-	log.Printf("answer size in bytes: %d", len(a))
+	answerLen := len(a)
+	log.Printf("answer size in bytes: %d", answerLen)
+	if s.experiment {
+		s.statsLogger.Printf("%d,%d", s.cores, answerLen)
+	}
 
 	return &proto.QueryResponse{Answer: a}, nil
 }
