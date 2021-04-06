65d213696e8a6f6f9e6fd40246006fa10de4be91
si-co <simone.colombo@epfl.ch>
2021-04-06 19:05:03 +0200

    Loop through schemes in real.sh

--- simulations/real.sh
+++ simulations/real.sh
@@ -22,27 +22,29 @@ cd - > /dev/null
 cd ../
 
 # run servers
-for cores in {1..8}; do
-  echo "##### running with $cores cores #####"
+for scheme in it dpf; do
+  for cores in {1..8}; do
+    echo "##### running with $cores cores #####"
 
-  # run servers
-  cmd/grpc/server/server -id=0 -files=1 -experiment -cores=$cores -scheme="it" >> simulations/results/stats_server-0.log & pid0=$!
-  cmd/grpc/server/server -id=1 -files=1 -experiment -cores=$cores -scheme="it" >> simulations/results/stats_server-1.log & pid1=$!
+    # run servers
+    cmd/grpc/server/server -id=0 -files=1 -experiment -cores=$cores -scheme=$scheme >> simulations/results/stats_server-0_$scheme.log & pid0=$!
+    cmd/grpc/server/server -id=1 -files=1 -experiment -cores=$cores -scheme=$scheme >> simulations/results/stats_server-1_$scheme.log & pid1=$!
 
-  # wait for server to setup
-  sleep 2
-  
-  # repeat experiment 10 times
-  for i in {1..2}; do
-    echo "##### iteration $i running with $cores cores #####"
-    cmd/grpc/client/client -id=alex.braulio@varidi.com -experiment -cores=$cores -scheme="it" >> simulations/results/stats_client.log
+    # wait for server to setup
+    sleep 2
+    
+    # repeat experiment 10 times
+    for i in {1..2}; do
+      echo "##### iteration $i running with $cores cores #####"
+      cmd/grpc/client/client -id=alex.braulio@varidi.com -experiment -cores=$cores -scheme=$scheme >> simulations/results/stats_client_$scheme.log
+      sleep 5
+    done
+
+    # send sigterm to servers and trigger graceful stop
+    kill -TERM "$pid0"
+    kill -TERM "$pid1"
+    echo "sleeping to let server gracefully stops..."
     sleep 5
+    echo "##### done with $cores #####"
   done
-
-  # send sigterm to servers and trigger graceful stop
-  kill -TERM "$pid0"
-  kill -TERM "$pid1"
-  echo "sleeping to let server gracefully stops..."
-  sleep 5
-  echo "##### done with $cores #####"
 done
