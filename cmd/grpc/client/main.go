e40e15ed12ef8f98623eec6c7f9dff7b5953293f
si-co <simone.colombo@epfl.ch>
2021-04-06 10:40:12 +0200

    Log client flags

--- cmd/grpc/client/main.go
+++ cmd/grpc/client/main.go
@@ -56,6 +56,7 @@ func newLocalClient() *localClient {
 		prg:   utils.RandomPRG(),
 		flags: parseFlags(),
 	}
+	log.Printf("cleint flags: %v", lc.flags)
 
 	// enable profiling if needed
 	if lc.flags.profiling {
