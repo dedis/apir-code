b6cb19868b4a82c1fca192baf60a859d21d14086
Kirill Nikitin <kirill.nikitin@epfl.ch>
2021-04-07 11:14:01 +0200

    Make the bar graph accommodate bw

--- simulations/plot.py
+++ simulations/plot.py
@@ -113,7 +113,8 @@ def plotVpirPerformanceBars():
     optimizationLabels = ["Matrix", "DPF"]
 
     fig, ax = plt.subplots()
-    ax.set_ylabel('Ratio to PIR Matrix latency', color='darkred')
+    # ax.set_ylabel('Ratio to PIR Matrix latency')
+    ax.set_ylabel('Ratio to PIR Matrix bandwidth')
     ax.set_xlabel('Database size [MB]')
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
@@ -131,17 +132,18 @@ def plotVpirPerformanceBars():
     for i, scheme in enumerate(schemes):
         stats = allStats(resultFolder + scheme)
         for j, dbSize in enumerate(sorted(stats.keys())):
-            Y = stats[dbSize]['client']['cpu']['mean'] + stats[dbSize]['server']['cpu']['mean']
+            # Y = stats[dbSize]['client']['cpu']['mean'] + stats[dbSize]['server']['cpu']['mean']
+            Y = stats[dbSize]['client']['bw']['mean'] + stats[dbSize]['server']['bw']['mean']
             if i == 0:
                 baselines[dbSize] = Y
             # Yerr = stats[dbSize]['client']['cpu']['std'] + stats[dbSize]['server']['cpu']['std']
             # bars[i] = ax.bar(j + i * width, Y, width, yerr=Yerr, color=colors[i % 3], hatch=patterns[int(i / 3)])
-            if i != 0:
-                bars[i] = ax.bar(j + i * width, Y / baselines[dbSize], width, color=colors[i % 3],
+            # if i != 0:
+            bars[i] = ax.bar(j + i * width, Y / baselines[dbSize], width, color=colors[i % 3],
                                  hatch=patterns[int(i / 3)])
-            else:
-                bars[i] = ax.bar(j + i * width, Y / baselines[dbSize], width, color='darkred',
-                                 hatch=patterns[int(i / 3)])
+            # else:
+            #     bars[i] = ax.bar(j + i * width, Y / baselines[dbSize], width, color='darkred',
+            #                      hatch=patterns[int(i / 3)])
             ax.annotate(rounder(Y / baselines[dbSize]),
                         xy=(j + i * width, Y / baselines[dbSize]),
                         xytext=(0, 0),  # 5 points vertical offset
@@ -151,6 +153,7 @@ def plotVpirPerformanceBars():
 
     ax.set_xticks(Xs + width * (len(schemes) - 1) / 2)
     ax.set_xticklabels(dbSizes)
+    ax.plot([0, len(Xs)], [1, 1], color='black')
 
     handles = []
     for i, label in enumerate(schemeLabels):
@@ -163,7 +166,8 @@ def plotVpirPerformanceBars():
     ax.legend(handles=handles, bbox_to_anchor=(0.01, 1.08, 0.94, 0.1), loc="upper left",
               mode="expand", borderaxespad=0, ncol=5)
     plt.tight_layout()
-    plt.savefig('multi_performance_bar_cpu.eps', format='eps', dpi=300, transparent=True)
+    # plt.savefig('multi_performance_bar_cpu.eps', format='eps', dpi=300, transparent=True)
+    plt.savefig('multi_performance_bar_bw.eps', format='eps', dpi=300, transparent=True)
     # plt.show()
 
 
@@ -185,8 +189,8 @@ def plotVpirPerformanceLines():
     ax.set_yscale('log')
     ax.yaxis.grid(True)
     ax.xaxis.grid(True)
-    ax.invert_xaxis()
-    ax.invert_yaxis()
+    # ax.invert_xaxis()
+    # ax.invert_yaxis()
 
     cpuTable = defaultdict(list)
     bwTable = defaultdict(list)
@@ -202,9 +206,15 @@ def plotVpirPerformanceLines():
             bwTable[dbSize].append(bw / 1000)
             Xs.append(1000 / cpu)
             Ys.append(GB / bw)
+            annotation = str(int(int(dbSize) / (8 * MB))) + "MB"
+            if int(dbSize) == 8 * GB:
+                annotation = "1GB"
+            ax.annotate(annotation, xy=(1000/cpu, GB/bw), xytext=(-20, 5),
+                        color=colors[i % int(len(schemes) / 2)], textcoords='offset points')
+            ax.plot(Xs[-1], Ys[-1], color=colors[i % int(len(schemes) / 2)], marker=".")
             # ax.annotate(str(int(int(dbSize) / (8 * MB))) + "MB", xy=(1000/cpu, GB/bw), xytext=(-20, 5),
             #             color=colors[i % int(len(schemes) / 2)], textcoords='offset points')
-            ax.plot(Xs[-1], Ys[-1], color=colors[i % int(len(schemes) / 2)], marker=markers[j])
+            # ax.plot(Xs[-1], Ys[-1], color=colors[i % int(len(schemes) / 2)], marker=markers[j])
 
         ax.plot(Xs, Ys, color=colors[i % int(len(schemes) / 2)],
                 linestyle=linestyles[int(i / (len(schemes) / 2))])
@@ -212,7 +222,7 @@ def plotVpirPerformanceLines():
     print_latex_table_separate(cpuTable, int(len(schemes) / 2))
     print_latex_table_separate(bwTable, int(len(schemes) / 2))
 
-    schemeLabels = ["PIR", "Merkle", "VPIR"]
+    schemeLabels = ["No integrity", "Merkle", "VPIR"]
     optimizationLabels = ["Matrix", "DPF"]
     handles = []
     for i, label in enumerate(schemeLabels):
@@ -220,10 +230,10 @@ def plotVpirPerformanceLines():
     for i, label in enumerate(optimizationLabels):
         handles.append(mlines.Line2D([], [], color='black',
                                      linestyle=linestyles[i], label=label))
-    for i, size in enumerate(dbSizes):
-        handles.append(mlines.Line2D([], [], color='black',
-                                     marker=markers[i], label=size))
-    ax.legend(handles=handles, loc='center right')
+    # for i, size in enumerate(dbSizes):
+    #     handles.append(mlines.Line2D([], [], color='black',
+    #                                  marker=markers[i], label=size))
+    ax.legend(handles=handles, loc='center left')
 
     # ax.annotate('Worse',
     #              xytext=(0.5, 1200),
@@ -233,8 +243,8 @@ def plotVpirPerformanceLines():
     # ax.legend(handles=handles, bbox_to_anchor=(0, 1.08, 1, 0.2), loc="lower left",
     #           mode="expand", borderaxespad=0, fancybox=True, ncol=3)
     plt.tight_layout()
-    # plt.savefig('multi_performance.eps', format='eps', dpi=300, transparent=True)
-    # plt.show()
+    plt.savefig('multi_performance.eps', format='eps', dpi=300, transparent=True)
+    plt.show()
 
 
 def plotSingle():
@@ -353,7 +363,7 @@ if __name__ == "__main__":
     if EXPR == "benchmarks":
         plotVpirBenchmarks()
     elif EXPR == "performance":
-        plotVpirPerformanceLines()
+        # plotVpirPerformanceLines()
         plotVpirPerformanceBars()
     elif EXPR == "single":
         plotSingle()
