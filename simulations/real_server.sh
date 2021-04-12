b07cd8c4af3f6ef413898835f9a3273d9fe0c542
si-co <simone.colombo@epfl.ch>
2021-04-12 18:34:54 +0200

    Update real_server

--- simulations/real_server.sh
+++ simulations/real_server.sh
@@ -6,7 +6,7 @@ rm results/stats*
 
 # build server
 cd ../cmd/grpc/server
-go build -race
+go build
 
 # go back to simultion directory
 cd - > /dev/null
@@ -17,10 +17,10 @@ cd ../
 # run servers
 for scheme in it dpf; do
   echo "##### running with $scheme scheme #####"
-  for cores in {1..12}; do
+  for cores in {1..24}; do
     echo "##### running with $cores cores #####"
     # run servers
-    cmd/grpc/server/server -id=$1 -files=1 -experiment -cores=$cores -scheme=$scheme | tee -a simulations/results/stats_server-0_$scheme.log
+    cmd/grpc/server/server -id=$1 -files=31 -experiment -cores=$cores -scheme=$scheme | tee -a simulations/results/stats_server-0_$scheme.log
     wait $!
 
     echo "##### done with $cores #####"
