3c0cb60c99719e15794e02bd12930150b3845d09
si-co <simone.colombo@epfl.ch>
2021-06-17 16:40:56 +0200

    128 bits test

--- lib/field/element_test.go
+++ lib/field/element_test.go
@@ -6,6 +6,40 @@ import (
 	"github.com/si-co/vpir-code/lib/utils"
 )
 
+func Benchmark128Bits(b *testing.B) {
+	// 4 elements of 32 bits each
+	rnd := utils.RandomPRG()
+	aa, err := new(Element).SetRandom(rnd)
+	if err != nil {
+		panic(err)
+	}
+	bb, err := new(Element).SetRandom(rnd)
+	if err != nil {
+		panic(err)
+	}
+	cc, err := new(Element).SetRandom(rnd)
+	if err != nil {
+		panic(err)
+	}
+	dd, err := new(Element).SetRandom(rnd)
+	if err != nil {
+		panic(err)
+	}
+
+	elements := []*Element{aa, bb, cc, dd}
+
+	r := new(Element).SetOne()
+
+	b.ResetTimer()
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		for _, e := range elements {
+			r.Mul(r, e)
+		}
+	}
+
+}
+
 func BenchmarkInnerProduct(b *testing.B) {
 	rnd1 := utils.RandomPRG()
 	rnd2 := utils.RandomPRG()
