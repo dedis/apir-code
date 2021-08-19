260a538b22bb525806d330c696b5410b2c42cf70
si-co <simone.colombo@epfl.ch>
2021-08-19 10:27:42 +0200

    Improve test

--- vpir_test.go
+++ vpir_test.go
@@ -3,10 +3,14 @@ package main
 // Test suite for integrated VPIR.
 
 import (
+	"fmt"
 	"io"
 	"testing"
 
+	"github.com/si-co/vpir-code/lib/client"
 	"github.com/si-co/vpir-code/lib/database"
+	"github.com/si-co/vpir-code/lib/monitor"
+	"github.com/si-co/vpir-code/lib/server"
 	"github.com/si-co/vpir-code/lib/utils"
 	"github.com/stretchr/testify/require"
 )
@@ -19,10 +23,40 @@ const (
 )
 
 func TestMultiBitVPIR(t *testing.T) {
+	keyToDownload := 500
 	numIdentifiers := 1000
+
 	rndDB := utils.RandomPRG()
-	_, err := database.CreateRandomDB(rndDB, numIdentifiers)
+	xof := utils.RandomPRG()
+	db, err := database.CreateRandomDB(rndDB, numIdentifiers)
 	require.NoError(t, err)
+
+	retrieveBlocksFSS(t, xof, db, keyToDownload, "FSSMultiBitVectorVPIR")
+}
+
+func retrieveBlocksFSS(t *testing.T, rnd io.Reader, db *database.DB, numBlocks int, testName string) {
+	c := client.NewFSS(rnd, &db.Info)
+	s0 := server.NewFSS(db, 0, c.Fss.PrfKeys)
+	s1 := server.NewFSS(db, 1, c.Fss.PrfKeys)
+
+	totalTimer := monitor.NewMonitor()
+	for i := 0; i < numBlocks; i++ {
+		fssKeys := c.Query(i, 2)
+
+		a0 := s0.Answer(fssKeys[0])
+		a1 := s1.Answer(fssKeys[1])
+
+		answers := [][]uint32{a0, a1}
+
+		res, err := c.Reconstruct(answers)
+		require.NoError(t, err)
+
+		elems := db.Range(i*db.BlockSize, (i+1)*db.BlockSize)
+
+		require.Equal(t, elems, res)
+	}
+
+	fmt.Printf("TotalCPU time %s: %.1fms\n", testName, totalTimer.Record())
 }
 
 func getXof(t *testing.T, key string) io.Reader {
