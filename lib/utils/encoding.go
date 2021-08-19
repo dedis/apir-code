f38d747cf2cd797d2ff1347fa575b4e29472aa29
si-co <simone.colombo@epfl.ch>
2021-08-19 15:34:22 +0200

    Remove unuseful function*

--- lib/utils/encoding.go
+++ lib/utils/encoding.go
@@ -2,17 +2,6 @@ package utils
 
 import "encoding/binary"
 
-func ByteSliceToUint32Slice(in []byte) []uint32 {
-	// since we suppose that this function is used only for decoding, we don't
-	// check for non-integer division here
-	out := make([]uint32, len(in)/4)
-	for i := range out {
-		out[i] = binary.BigEndian.Uint32(in[i*4 : (i+1)*4])
-	}
-
-	return out
-}
-
 func Uint32SliceToByteSlice(in []uint32) []byte {
 	out := make([]byte, len(in)*4)
 	for i := range in {
