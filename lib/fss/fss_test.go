d8b28ebaf6535aa25bc5dc4e4effd215a876ebda
si-co <simone.colombo@epfl.ch>
2021-08-24 10:53:11 +0200

    Update FSS test

--- lib/fss/fss_test.go
+++ lib/fss/fss_test.go
@@ -6,14 +6,17 @@ import (
 	"testing"
 
 	"github.com/si-co/vpir-code/lib/field"
+	"github.com/si-co/vpir-code/lib/utils"
 	"github.com/stretchr/testify/require"
 )
 
-const testBlockLength = 16
+const (
+	testBlockLength = 17
+	numBits         = uint(10)
+)
 
 func TestPoint(t *testing.T) {
 	// Generate fss Keys on client
-	numBits := uint(32)
 	fClient := ClientInitialize(numBits, testBlockLength)
 	index := field.RandElement()
 	// Test with if x = index, evaluate to vector b
@@ -22,6 +25,46 @@ func TestPoint(t *testing.T) {
 	for i := range b {
 		b[i] = field.RandElement()
 	}
+	fssKeys := fClient.GenerateTreePF(index, b)
+
+	// Simulate server
+	fServer := ServerInitialize(fClient.PrfKeys, fClient.NumBits)
+
+	zeros := make([]uint32, bLen)
+	for j := uint32(0); j < (1 << numBits); j++ {
+		out0 := make([]uint32, bLen)
+		out1 := make([]uint32, bLen)
+		sum := make([]uint32, bLen)
+
+		fServer.EvaluatePF(0, fssKeys[0], j, out0)
+		fServer.EvaluatePF(1, fssKeys[1], j, out1)
+
+		for i := range sum {
+			sum[i] = (out0[i] + out1[i]) % field.ModP
+		}
+
+		if j == index {
+			require.Equal(t, b, sum)
+		} else {
+			require.Equal(t, zeros, sum)
+		}
+	}
+}
+
+func TestPointWithAlphaVector(t *testing.T) {
+	// Generate fss Keys on client
+	fClient := ClientInitialize(numBits, testBlockLength)
+	index := field.RandElement()
+	// Test with if x = index, evaluate to vector b
+	alpha := field.RandElementWithPRG(utils.RandomPRG())
+	bLen := testBlockLength
+	b := make([]uint32, testBlockLength)
+	b[0] = 1
+	for i := 1; i < len(b); i++ {
+		a := (uint64(b[i-1]) * uint64(alpha)) % uint64(field.ModP)
+		b[i] = uint32(a)
+	}
+
 	fssKeys := fClient.GenerateTreePF(index, b)
 
 	// Simulate server
