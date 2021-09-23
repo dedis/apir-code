461a00c08ccd52e4ca5142dc838a83fb56f45955
si-co <simone.colombo@epfl.ch>
2021-09-23 11:28:10 +0200

    Update run_all.sh script

--- simulations/run_all.sh
+++ simulations/run_all.sh
@@ -1,24 +1,5 @@
 #!/usr/bin/env bash
 
-## prepend general simulation parameters to specific simulation parameters
-#for file in $(find . -maxdepth 1 -iname '*.toml' -not -name 'simul.toml'); do
-#  cat simul.toml >> $file
-#done
-#
-## trap CTRL-c and call ctrl_c()
-#trap clean INT
-#
-## horrible hack to keep specific simulation's toml files clean
-#function clean() {
-#  tmpfile=$(mktemp)
-#  for file in $(find . -maxdepth 1 -iname '*.toml' -not -name 'simul.toml'); do
-#    head -n -$(wc -l < simul.toml) $file > $tmpfile
-#    cat ${tmpfile} > $file
-#  done
-#  rm -f ${tmpfile}
-#  exit
-#}
-
 # create results dir if not already present
 mkdir -p results
 
