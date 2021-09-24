3caf860d4722bd69502572855884e618d4b77229
si-co <simone.colombo@epfl.ch>
2021-09-24 13:25:44 +0200

    Update real.sh

--- simulations/real.sh
+++ simulations/real.sh
@@ -6,14 +6,14 @@ rm results/stats*
 
 # build server
 cd ../cmd/grpc/server
-go build -race
+go build
 
 # go back to simultion directory
 cd - > /dev/null
 
 # build client
 cd ../cmd/grpc/client
-go build -race
+go build
 
 # go back to simultion directory
 cd - > /dev/null
@@ -22,30 +22,34 @@ cd - > /dev/null
 cd ../
 
 # run servers
-for scheme in it dpf; do
+for scheme in pointPIR, pointVPIR; do
   echo "##### running with $scheme scheme #####"
-  for cores in {1..12}; do
-    echo "##### running with $cores cores #####"
+  #for cores in {1..12}; do
+  #echo "##### running with $cores cores #####"
 
-    # run servers
-    cmd/grpc/server/server -id=0 -files=1 -experiment -cores=$cores -scheme=$scheme >> simulations/results/stats_server-0_$scheme.log & pid0=$!
-    cmd/grpc/server/server -id=1 -files=1 -experiment -cores=$cores -scheme=$scheme >> simulations/results/stats_server-1_$scheme.log & pid1=$!
+  # run servers
+  #cmd/grpc/server/server -id=0 -files=1 -experiment -cores=$cores -scheme=$scheme >> simulations/results/stats_server-0_$scheme.log & pid0=$!
+  #cmd/grpc/server/server -id=1 -files=1 -experiment -cores=$cores -scheme=$scheme >> simulations/results/stats_server-1_$scheme.log & pid1=$!
+  cmd/grpc/server/server -id=0 -files=1 -experiment -scheme=$scheme >> simulations/results/stats_server-0_$scheme.log & pid0=$!
+  cmd/grpc/server/server -id=1 -files=1 -experiment -scheme=$scheme >> simulations/results/stats_server-1_$scheme.log & pid1=$!
 
-    # wait for server to setup
-    sleep 2
-    
-    # repeat experiment 10 times
-    for i in {1..2}; do
-      echo "##### iteration $i running with $cores cores #####"
-      cmd/grpc/client/client -id=alex.braulio@varidi.com -experiment -cores=$cores -scheme=$scheme >> simulations/results/stats_client_$scheme.log
-      sleep 5
-    done
-
-    # send sigterm to servers and trigger graceful stop
-    kill -TERM "$pid0"
-    kill -TERM "$pid1"
-    echo "sleeping to let server gracefully stops..."
+  # wait for server to setup
+  sleep 2
+  
+  # repeat experiment 10 times
+  for i in {1..10}; do
+    #echo "##### iteration $i running with $cores cores #####"
+    echo "##### iteration $i #####"
+    #cmd/grpc/client/client -id=alex.braulio@varidi.com -experiment -cores=$cores -scheme=$scheme >> simulations/results/stats_client_$scheme.log
+    cmd/grpc/client/client -id=alex.braulio@varidi.com -experiment -scheme=$scheme >> simulations/results/stats_client_$scheme.log
     sleep 5
-    echo "##### done with $cores #####"
   done
+
+  # send sigterm to servers and trigger graceful stop
+  kill -TERM "$pid0"
+  kill -TERM "$pid1"
+  echo "sleeping to let server gracefully stops..."
+  sleep 5
+  echo "##### done with $cores #####"
+  #done
 done
