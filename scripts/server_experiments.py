11fd6a19cee81539e4db6e3d76d2a65c4eb722ff
si-co <simone.colombo@epfl.ch>
2021-12-15 20:19:23 +0100

    Add python server to automatize experiments

---
+++ scripts/server_experiments.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+
+#### modified
+"""
+Very simple HTTP server in python (Updated for Python 3.7)
+
+Usage:
+
+    ./dummy-web-server.py -h
+    ./dummy-web-server.py -l localhost -p 8000
+
+Send a GET request:
+
+    curl http://localhost:8000
+
+Send a HEAD request:
+
+    curl -I http://localhost:8000
+
+Send a POST request:
+
+    curl -d "foo=bar&bin=baz" http://localhost:8000
+
+This code is available for use under the MIT license.
+
+----
+
+Copyright 2021 Brad Montgomery
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
+associated documentation files (the "Software"), to deal in the Software without restriction, 
+including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial 
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT 
+LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.    
+
+"""
+import argparse
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import os
+
+
+class S(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+    def _html(self, message):
+        """This just generates an HTML document that includes `message`
+        in the body. Override, or re-write this do do more interesting stuff.
+
+        """
+        content = f"<html><body><h1>{message}</h1></body></html>"
+        return content.encode("utf8")  # NOTE: must return a bytes object!
+
+    def do_GET(self):
+        self._set_headers()
+        self.wfile.write(self._html("hi!"))
+
+    def do_HEAD(self):
+        self._set_headers()
+
+    def do_POST(self):
+        # Doesn't do anything with posted data
+        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+        post_data = self.rfile.read(content_length) # <--- Gets the data itself
+        data = bytes.decode(post_data).split(",")
+        os.environ['SCHEME'] = data[0]
+        os.environ['TARGET'] = data[1]
+        print(os.environ['SCHEME'])
+        print(os.environ['TARGET'])
+
+
+def run(server_class=HTTPServer, handler_class=S, addr="localhost", port=8000):
+    server_address = (addr, port)
+    httpd = server_class(server_address, handler_class)
+
+    print(f"Starting httpd server on {addr}:{port}")
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    # set ENV variables
+    os.environ['SCHEME'] = ''
+    os.environ['TARGET'] = ''
+
+    parser = argparse.ArgumentParser(description="Run a simple HTTP server")
+    parser.add_argument(
+        "-l",
+        "--listen",
+        default="localhost",
+        help="Specify the IP address on which the server listens",
+    )
+    parser.add_argument(
+        "-p",
+        "--port",
+        type=int,
+        default=8000,
+        help="Specify the port on which the server listens",
+    )
+    args = parser.parse_args()
+    run(addr=args.listen, port=args.port)
+
