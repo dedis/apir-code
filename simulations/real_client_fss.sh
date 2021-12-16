e7444f12943f6961b9a66ad3cd27be33f14a9d7a
si-co <simone.colombo@epfl.ch>
2021-12-16 10:42:15 +0100

    Revert "Exp"
    This reverts commit f14eb1e2c7924ea6ea4e29212bfa2f771f6c881b.

--- simulations/real_client_fss.sh
+++ simulations/real_client_fss.sh
@@ -16,52 +16,55 @@ cd - > /dev/null
 # move to root
 cd ../
 
+scheme="$1"
+echo "##### running with $scheme scheme #####"
+
 # repeat experiment 30 times
 repeat=20
 
-for scheme in "complexPIR" "complexVPIR"; do
-  echo "##### running with $scheme scheme #####"
-
-  target="email"
-  # init ENV var on the servers
-  curl -d "${scheme},${target}" 10.90.36.38:8000
-  curl -d "${scheme},${target}" 10.90.36.39:8000
-
-  # run experiment
-  for i in {1..$repeat}; do
-    echo "    ##### iteration $i with scheme $scheme"
-    cmd/grpc/client/client -id=".edu" -target=$target -from-end=4 -experiment -scheme=$scheme | tee -a simulations/results/stats_client_${scheme}_${target}.log
-    sleep 5
-  done
-
-  target="algo"
-  # init ENV var on the servers
-  curl -d "${scheme},${target}" 10.90.36.38:8000
-  curl -d "${scheme},${target}" 10.90.36.39:8000
-  for i in {1..$repeat}; do
-    echo "    ##### iteration $i with scheme $scheme"
-    cmd/grpc/client/client -id="ElGamal" -target=$target -experiment -scheme=$scheme | tee -a simulations/results/stats_client_${scheme}_${target}.log
-    sleep 5
-  done
-
-  # THIS IS FOR THE AND QUERY, HARDCODED IN GO
-  target="and"
-  curl -d "${scheme},${target}" 10.90.36.38:8000
-  curl -d "${scheme},${target}" 10.90.36.39:8000
-  for i in {1..$repeat}; do
-    echo "    ##### iteration $i with scheme $scheme"
-    cmd/grpc/client/client -and -experiment -scheme=$scheme | tee -a simulations/results/stats_client_${scheme}_and.log
-    sleep 5
-  done
-
-  # THIS IS FOR THE AVG QUERY, HARDCODED IN GO
-  target="avg"
-  curl -d "${scheme},${target}" 10.90.36.38:8000
-  curl -d "${scheme},${target}" 10.90.36.39:8000
-  for i in {1..$repeat}; do
-    echo "    ##### iteration $i with scheme $scheme"
-    cmd/grpc/client/client -and -avg -experiment -scheme=$scheme | tee -a simulations/results/stats_client_${scheme}_and.log
-    sleep 5
-  done
-sleep 10
+target="email"
+for i in {1..$repeat}; do
+  echo "    ##### iteration $i"
+  cmd/grpc/client/client -id=".edu" -target=$target -from-end=4 -experiment -scheme=$scheme | tee -a simulations/results/stats_client_${scheme}_${target}.log
+  sleep 5
 done
+
+#target="algo_elgamal"
+#for i in {1..$repeat}; do
+  #echo "    ##### iteration $i"
+  #cmd/grpc/client/client -id="ElGamal" -target=$target -experiment -scheme=$scheme | tee -a simulations/results/stats_client_${scheme}_${target}.log
+  #sleep 5
+#done
+
+#target="creation"
+#for i in {1..$repeat}; do
+  #echo "    ##### iteration $i"
+  #cmd/grpc/client/client -id="2020" -target=$target -experiment -scheme=$scheme | tee -a simulations/results/stats_client_${scheme}_${target}.log
+  #sleep 5
+#done
+
+# THIS IS FOR THE AND QUERY, HARDCODED IN GO
+#for i in {1..$repeat}; do
+  #echo "    ##### iteration $i"
+  #cmd/grpc/client/client -and -experiment -scheme=$scheme | tee -a simulations/results/stats_client_${scheme}_and.log
+  #sleep 5
+#done
+
+## THIS IS FOR THE AND QUERY, HARDCODED IN GO
+#for i in {1..$repeat}; do
+  #echo "    ##### iteration $i"
+  #cmd/grpc/client/client -and -experiment -scheme=$scheme | tee -a simulations/results/stats_client_${scheme}_and.log
+  #sleep 5
+#done
+
+## THIS IS FOR THE AVG QUERY, HARDCODED IN GO
+#for i in {1..$repeat}; do
+  #echo "    ##### iteration $i"
+  #cmd/grpc/client/client -and -avg -experiment -scheme=$scheme | tee -a simulations/results/stats_client_${scheme}_and.log
+  #sleep 5
+#done
+
+# terminates servers
+curl 10.90.36.38:8080 > /dev/null
+curl 10.90.36.39:8080 > /dev/null
+sleep 10
