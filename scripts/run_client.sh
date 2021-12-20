57423568739bfeedaeb66870a414f214ef08aa27
si-co <simone.colombo@epfl.ch>
2021-12-20 11:54:07 +0100

    Fix local run_client script

--- scripts/run_client.sh
+++ scripts/run_client.sh
@@ -9,4 +9,6 @@ cd - > /dev/null
 cd ../
 
 #cmd/grpc/client/client -id=resch@ocg.at -scheme=$1
-cmd/grpc/client/client -id=".ch" -target="email" -from-end="3" -scheme=$1
+#cmd/grpc/client/client -id=".ch" -target="email" -from-end="3" -scheme=$1
+cmd/grpc/client/client -id=".edu" -from-end=4 -and -avg -scheme=$1
+#cmd/grpc/client/client -id=".edu" -from-end=4 -and -scheme=$1
