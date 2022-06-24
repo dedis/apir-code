16ead966790ec3e4c5cb7a0335f032ebd9c79e84
si-co <simone.colombo@epfl.ch>
2022-06-24 16:05:44 +0200

    add tests for multiple servers

--- merkle_test.go
+++ merkle_test.go
@@ -20,6 +20,7 @@ import (
 )
 
 func TestMatrixOneMbMerkle(t *testing.T) {
+	numServers := 2
 	dbLen := oneMB
 	blockLen := testBlockLength * field.Bytes
 	// since this scheme works on bytes, the bit size of one element is 8
@@ -35,27 +36,49 @@ func TestMatrixOneMbMerkle(t *testing.T) {
 	db := database.CreateRandomMerkle(xofDB, dbLen, nRows, blockLen)
 	fmt.Println("DB created")
 
-	retrieveBlocksITMerkle(t, xof, db, numBlocks, "MatrixOneMbMerkle")
+	retrieveBlocksITMerkle(t, xof, db, numServers, numBlocks, "MatrixOneMbMerkle")
 }
 
-func retrieveBlocksITMerkle(t *testing.T, rnd io.Reader, db *database.Bytes, numBlocks int, testName string) {
+func TestMatrixOneMbMerkleFourServers(t *testing.T) {
+	numServers := 4
+	dbLen := oneMB
+	blockLen := testBlockLength * field.Bytes
+	// since this scheme works on bytes, the bit size of one element is 8
+	elemBitSize := 8
+	numBlocks := dbLen / (elemBitSize * blockLen)
+	nCols := int(math.Sqrt(float64(numBlocks)))
+	nRows := nCols
+
+	// functions defined in vpir_test.go
+	xofDB := utils.RandomPRG()
+	xof := utils.RandomPRG()
+
+	db := database.CreateRandomMerkle(xofDB, dbLen, nRows, blockLen)
+	fmt.Println("DB created")
+
+	retrieveBlocksITMerkle(t, xof, db, numServers, numBlocks, "MatrixOneMbMerkle")
+}
+
+func retrieveBlocksITMerkle(t *testing.T, rnd io.Reader, db *database.Bytes, numServers, numBlocks int, testName string) {
 	c := client.NewPIR(rnd, &db.Info)
-	s0 := server.NewPIR(db)
-	s1 := server.NewPIR(db)
+	servers := make([]*server.PIR, numServers)
+	for i := range servers {
+		servers[i] = server.NewPIR(db)
+	}
 
 	totalTimer := monitor.NewMonitor()
 	for i := 0; i < numBlocks; i++ {
 		in := make([]byte, 4)
 		binary.BigEndian.PutUint32(in, uint32(i))
-		queries, err := c.QueryBytes(in, 2)
+		queries, err := c.QueryBytes(in, numServers)
 		require.NoError(t, err)
 
-		a0, err := s0.AnswerBytes(queries[0])
-		require.NoError(t, err)
-		a1, err := s1.AnswerBytes(queries[1])
-		require.NoError(t, err)
-
-		answers := [][]byte{a0, a1}
+		answers := make([][]byte, numServers)
+		for i, s := range servers {
+			a, err := s.AnswerBytes(queries[i])
+			require.NoError(t, err)
+			answers[i] = a
+		}
 
 		res, err := c.ReconstructBytes(answers)
 		require.NoError(t, err)
