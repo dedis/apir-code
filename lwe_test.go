7537ef6df515c4d82cf85cfed98548ff65910199
si-co <simone.colombo@epfl.ch>
2022-07-06 09:05:43 +0200

    refactor tests

--- lwe_test.go
+++ lwe_test.go
@@ -1,41 +1,48 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"testing"
 
 	"github.com/si-co/vpir-code/lib/client"
 	"github.com/si-co/vpir-code/lib/database"
+	"github.com/si-co/vpir-code/lib/monitor"
 	"github.com/si-co/vpir-code/lib/server"
 	"github.com/si-co/vpir-code/lib/utils"
 	"github.com/stretchr/testify/require"
 )
 
+func TestLWEMatrixOneMb(t *testing.T) {
+	p := utils.ParamsWithDatabaseSize(1024, 1024)
+	db := database.CreateRandomBinaryWithSizeLWE(utils.RandomPRG(), p.L, p.M)
+	retrieveBlocksLWE(t, db, p, "SingleMatrixLWEOneMb")
+}
+
 func TestDefaultLWE(t *testing.T) {
 	// get default parameters
 	p := utils.ParamsDefault()
-
-	// create random database with default parameters
 	db := database.CreateRandomBinaryLWE(utils.RandomPRG(), p.L, p.M)
+	retrieveBlocksLWE(t, db, p, "SingleDefaultLWE")
+}
 
-	// initialize client
-	c := client.NewLWE(utils.RandomPRG(), &db.Info)
-
-	// initialize server
+func retrieveBlocksLWE(t *testing.T, db *database.LWE, params *utils.ParamsLWE, testName string) {
+	c := client.NewLWE(utils.RandomPRG(), &db.Info, params)
 	s := server.NewLWE(db)
 
-	// query
-	for k := 0; k < 100; k++ {
-		i := rand.Intn(p.L * p.M)
-		q, err := c.QueryBytes(i)
+	totalTimer := monitor.NewMonitor()
+	for j := 0; j < 10; j++ {
+		i := rand.Intn(params.L * params.M)
+		query, err := c.QueryBytes(i)
 		require.NoError(t, err)
 
-		a, err := s.AnswerBytes(q)
+		a, err := s.AnswerBytes(query)
 		require.NoError(t, err)
 
 		res, err := c.ReconstructBytes(a)
 		require.NoError(t, err)
-
 		require.Equal(t, db.Matrix.Get(i/db.Info.NumColumns, i%db.Info.NumColumns), res)
 	}
+	fmt.Printf("\nTotalCPU time %s: %.1fms\n", testName, totalTimer.Record())
+
 }
