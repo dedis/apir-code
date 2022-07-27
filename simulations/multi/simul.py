dd1a2c18e8c270b14a68100e5322eaa4afae325b
si-co <simone.colombo@epfl.ch>
2022-07-27 17:02:47 +0200

    WIP multi-machine eval

--- simulations/multi/simul.py
+++ simulations/multi/simul.py
@@ -1,6 +1,8 @@
+import multiprocessing
+from multiprocessing import Process
 import os
 import tomli
-from fabric import SerialGroup as Group
+from fabric import ThreadingGroup as Group
 
 user = os.getenv('APIR_USER')
 password = os.getenv('APIR_PASSWORD')
@@ -55,8 +57,14 @@ def server_pir_merkle(c, sid, dbLen, nRows, blockLen):
 def experiment_pir_classic(p):
     gc = load_general_config()
     ic = load_individual_config('pirClassic.toml')
-    for c in p:
-        server_pir_classic(c, 0, gc['DBBitLengths'][0], ic['NumRows'], ic['BlockLength'])
+    first = [p[0], 0, gc['DBBitLengths'][0], ic['NumRows'], ic['BlockLength']]
+    second = [p[1], 1, gc['DBBitLengths'][0], ic['NumRows'], ic['BlockLength']]
+    arguments = [first, second]
+    ppool = multiprocessing.Pool(2)
+    ppool.map(server_pir_classic, arguments)
+    # for i, c in enumerate(p):
+        # print(i)
+        # server_pir_classic(c, 0, gc['DBBitLengths'][0], ic['NumRows'], ic['BlockLength'])
 
 pool = servers_pool()
 for c in pool:
