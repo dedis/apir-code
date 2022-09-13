0ca31f5d22c09de4cd3d9f57fb55df697e1a9efa
si-co <simone.colombo@epfl.ch>
2022-09-13 11:25:10 +0200

    fix script for t values in integrity amplification

--- scripts/integrity_amplification.py
+++ scripts/integrity_amplification.py
@@ -2,29 +2,21 @@ import math
 
 sigma = 6.4
 secparam = 128
-db_sizes = [1024, 1048576, 1073741824] # in bits
+q = pow(2, 32)
+db_sizes = [8192, 8389000, 8590000000]
 
 bounds = []
 
 for dbs in db_sizes:
     bounds.append(math.ceil(math.sqrt(dbs * secparam) * sigma))
 
-q = pow(2, 32)
-
 epsilons = []
 for B in bounds:
     epsilons.append((2*B - 1)/(q - 4*B + 1))
 
-print("integrity error 2^-128")
-for i, epsilon in enumerate(epsilons):
-    for t in range(1, 15):
-        if pow(epsilon, t + 1) < 1/pow(2, 128):
-            print("\t DB size:", db_sizes[i], "B: ", bounds[i], "t: ", t)
-            break
-
 print("integrity error 2^-64")
 for i, epsilon in enumerate(epsilons):
     for t in range(1, 15):
-        if pow(epsilon, t + 1) < 1/pow(2, 64):
+        if pow(epsilon, t + 1) <= 1/pow(2, 64):
             print("\t DB size:", db_sizes[i], "B: ", bounds[i], "t: ", t)
             break
