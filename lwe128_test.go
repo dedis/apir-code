7e340709ff8797fcedc68c28cff4c1d2ab96f9f2
si-co <simone.colombo@epfl.ch>
2022-09-16 16:20:28 +0200

    update LWE128 test

--- lwe128_test.go
+++ lwe128_test.go
@@ -4,10 +4,10 @@ import (
 	"fmt"
 	"math/rand"
 	"testing"
+	"time"
 
 	"github.com/si-co/vpir-code/lib/client"
 	"github.com/si-co/vpir-code/lib/database"
-	"github.com/si-co/vpir-code/lib/monitor"
 	"github.com/si-co/vpir-code/lib/server"
 	"github.com/si-co/vpir-code/lib/utils"
 	"github.com/stretchr/testify/require"
@@ -15,6 +15,7 @@ import (
 
 func TestLWEMatrixOneMb128(t *testing.T) {
 	dbLen := 1024 * 1024 // dbLen is specified in bits
+	//dbLen := 4 * 4
 	db := database.CreateRandomBinaryLWEWithLength128(utils.RandomPRG(), dbLen)
 	p := utils.ParamsWithDatabaseSize128(db.Info.NumRows, db.Info.NumColumns)
 	retrieveBlocksLWE128(t, db, p, "SingleMatrixLWEOneMb_128")
@@ -24,8 +25,8 @@ func retrieveBlocksLWE128(t *testing.T, db *database.LWE128, params *utils.Param
 	c := client.NewLWE128(utils.RandomPRG(), &db.Info, params)
 	s := server.NewLWE128(db)
 
-	totalTimer := monitor.NewMonitor()
-	for j := 0; j < 10; j++ {
+	ti := time.Now()
+	for j := 0; j < 100; j++ {
 		i := rand.Intn(params.L * params.M)
 		query, err := c.QueryBytes(i)
 		require.NoError(t, err)
@@ -37,5 +38,5 @@ func retrieveBlocksLWE128(t *testing.T, db *database.LWE128, params *utils.Param
 		require.NoError(t, err)
 		require.Equal(t, uint32(db.Matrix.Get(utils.VectorToMatrixIndices(i, db.Info.NumColumns))), res)
 	}
-	fmt.Printf("TotalCPU time %s: %.1fms\n", testName, totalTimer.Record())
+	fmt.Printf("TotalCPU time %s: %.1fs\n", testName, time.Since(ti).Seconds())
 }
