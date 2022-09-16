45a63fe28619dbe97cb0af2a127aa9744f37029a
si-co <simone.colombo@epfl.ch>
2022-09-16 16:28:12 +0200

    move to bytes for LWE128 db

--- lib/database/lwe128.go
+++ lib/database/lwe128.go
@@ -5,16 +5,15 @@ import (
 
 	"github.com/si-co/vpir-code/lib/matrix"
 	"github.com/si-co/vpir-code/lib/utils"
-	"lukechampine.com/uint128"
 )
 
 type LWE128 struct {
-	Matrix *matrix.Matrix128
+	Matrix *matrix.MatrixBytes
 	Info
 }
 
 func Digest128(db *LWE128, rows int) *matrix.Matrix128 {
-	return matrix.Mul128(
+	return matrix.BinaryMul128(
 		matrix.NewRandom128(
 			utils.NewPRG(utils.ParamsDefault128().SeedA),
 			utils.ParamsDefault128().N,
@@ -28,19 +27,21 @@ func CreateRandomBinaryLWEWithLength128(rnd io.Reader, dbLen int) *LWE128 {
 }
 
 func CreateRandomBinaryLWE128(rnd io.Reader, numRows, numColumns int) *LWE128 {
-	m := matrix.New128(numRows, numColumns)
+	m := matrix.NewBytes(numRows, numColumns)
 	// read random bytes for filling out the entries
-	// For simplicity, we use the whole byte to store 0 or 1
-	data := make([]byte, numRows*numColumns)
+	// the +1 takes into account a float division by 8
+	// CHECK +1
+	data := make([]byte, numRows*numColumns/8)
 	if _, err := rnd.Read(data); err != nil {
 		panic(err)
 	}
 
-	for i := 0; i < numRows; i++ {
-		for j := 0; j < numColumns; j++ {
-			val := uint128.From64(uint64(data[i] & 1))
-			m.Set(i, j, val)
+	for i := 0; i < m.Len(); i++ {
+		val := (data[i/8] >> (i % 8)) & 1
+		if val >= plaintextModulus {
+			panic("value too large")
 		}
+		m.SetData(i, val)
 	}
 
 	db := &LWE128{
