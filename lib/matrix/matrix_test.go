d67e6ae0a0815f0a8a7176b39e5a84306c3cb709
si-co <simone.colombo@epfl.ch>
2022-09-18 20:46:37 +0200

    add benchmarking for 2^32 mod

--- lib/matrix/matrix_test.go
+++ lib/matrix/matrix_test.go
@@ -1,8 +1,10 @@
 package matrix
 
 import (
+	"fmt"
 	"testing"
 
+	"github.com/si-co/vpir-code/lib/utils"
 	"github.com/stretchr/testify/require"
 )
 
@@ -31,4 +33,31 @@ func TestEncoding(t *testing.T) {
 	for i := range mmRec {
 		require.Equal(t, &mm[i], &mmRec[i])
 	}
+}
+
+func BenchmarkBinaryMul(b *testing.B) {
+	rows, columns := 1024, 1024
+	buff := make([]byte, rows*columns/8+1)
+	rnd := utils.RandomPRG()
+	if _, err := rnd.Read(buff); err != nil {
+		panic("insufficient randomness")
+	}
+
+	m := NewBytes(rows, columns)
+	for i := 0; i < m.Len(); i++ {
+		val := (buff[i/8] >> (i % 8)) & 1
+		m.SetData(i, val)
+	}
+
+	rm := NewRandom(
+		utils.NewPRG(utils.ParamsDefault().SeedA),
+		utils.ParamsDefault().N,
+		rows)
+
+	for i := 0; i < b.N; i++ {
+		d := BinaryMul(rm, m)
+		// to avoid compiler optimization
+		fmt.Println(d.Rows())
+	}
+
 }
