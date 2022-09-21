ea9ac1fce6506d8bfed5b35742f758ca0dd675df
si-co <simone.colombo@epfl.ch>
2022-09-21 11:27:41 +0200

    improve binary mul

--- lib/matrix/matrix.c
+++ lib/matrix/matrix.c
@@ -16,9 +16,10 @@ void binary_multiply(int aRows, int aCols, int bCols, uint32_t *a, uint8_t *b, u
 	for (i = 0; i < aRows; i++) {
 		for (k = 0; k < aCols; k++) {
 			for (j = 0; j < bCols; j++) {
-				if (b[bCols*k+j] == 1) {
-					out[bCols*i+j] += a[aCols*i+k];
-				}
+				//if (b[bCols*k+j] == 1) {
+				//out[bCols*i+j] += a[aCols*i+k];
+				//}
+				out[bCols*i+j] += a[aCols*i+k] & -b[bCols*k+j];
 			}
 		}
 	}
@@ -40,9 +41,10 @@ void binary_multiply128(int aRows, int aCols, int bCols, __uint128_t *a, uint8_t
 	for (i = 0; i < aRows; i++) {
 		for (k = 0; k < aCols; k++) {
 			for (j = 0; j < bCols; j++) {
-				if (b[bCols*k+j] == 1) {
-					out[bCols*i+j] += a[aCols*i+k];
-				}
+				//if (b[bCols*k+j] == 1) {
+				//out[bCols*i+j] += a[aCols*i+k];
+				//}
+				out[bCols*i+j] += a[aCols*i+k] & -b[bCols*k+j];
 			}
 		}
 	}
