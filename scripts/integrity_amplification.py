843f2365436443b15032f99e0e873a268bfdd54c
si-co <simone.colombo@epfl.ch>
2022-09-27 17:14:56 +0200

    minor

--- scripts/integrity_amplification.py
+++ scripts/integrity_amplification.py
@@ -3,7 +3,7 @@ import math
 sigma = 6.4
 secparam = 128
 q = pow(2, 32)
-db_sizes = [8192, 8389000, 8590000000]
+db_sizes = [1 << 13, 1 << 23, 1 << 33]
 
 bounds = []
 
@@ -13,6 +13,13 @@ for dbs in db_sizes:
 epsilons = []
 for B in bounds:
     epsilons.append((2*B - 1)/(q - 4*B + 1))
+
+print("integrity error 2^-128")
+for i, epsilon in enumerate(epsilons):
+    for t in range(1, 20):
+        if pow(epsilon, t + 1) <= 1/pow(2, 128):
+            print("\t DB size:", db_sizes[i], "B: ", bounds[i], "t: ", t)
+            break
 
 print("integrity error 2^-64")
 for i, epsilon in enumerate(epsilons):
