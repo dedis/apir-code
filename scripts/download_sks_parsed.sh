5a4dfbfcc26b0f290abe65ffd74bf93d5811984e
si-co <simone.colombo@epfl.ch>
2023-02-27 12:24:13 +0100

    update script

--- scripts/download_sks_parsed.sh
+++ scripts/download_sks_parsed.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 BASE_FOLDER="data"
-SKS_FOLDER="sks-tmp"
+SKS_FOLDER="sks"
 cd $BASE_FOLDER
 mkdir -p $SKS_FOLDER && cd $_
-wget --no-check-certificate "https://1drv.ms/u/s!Anbw-PjTZVKKkBu-tGVJDIe0q_L4?e=7m83gq"
-unzip sks.zip
+for i in {00..30}; do 
+  wget --content-disposition https://drive.switch.ch/index.php/s/jejfxB9uJRUAmqm/download\?path\=%2F\&files\=sks-0$i.pgp 
+done
