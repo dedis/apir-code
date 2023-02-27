f51baa25911f1bd258eb6a0c5b66e54e1be8f796
si-co <simone.colombo@epfl.ch>
2023-02-27 11:43:54 +0100

    save

--- lib/database/elliptic.go
+++ lib/database/elliptic.go
@@ -6,7 +6,6 @@ import (
 	"io"
 	"log"
 	"math"
-	"runtime"
 
 	"github.com/cloudflare/circl/group"
 	"github.com/si-co/vpir-code/lib/utils"
@@ -28,10 +27,7 @@ func CreateRandomEllipticWithDigest(rnd io.Reader, dbLen int, g group.Group, reb
 	for i := 0; i < len(data); i++ {
 		data[i] = data[i] & 1
 	}
-	NGoRoutines := runtime.NumCPU()
-	if numRows < 1000 { // dirty hack for small databases
-		NGoRoutines = 8
-	}
+	NGoRoutines := 8
 	h := crypto.BLAKE2b_256
 	rowsPerRoutine := int(math.Ceil(float64(numRows) / float64(NGoRoutines)))
 	replies := make([]chan []byte, NGoRoutines)
